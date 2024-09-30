552e224039382b22609f5853877c9a51b7159a82
meldridge <meldridge@users.noreply.github.com>
2024-09-30 22:56:53 +0930

    Got WASD rotation working

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 
 # Local imports
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
@@ -10,17 +11,33 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    # Initialise clock
+    clock = pygame.time.Clock()
+    dt = 0
+
+    # Initialise screen
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # Initialise Player in the middle of the screen
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while(True):
         # Get events
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
+            
         # Refresh screen
-        screen.fill(pygame.Color(0,0,0,))
+        screen.fill(pygame.Color("black"))
+
+        # Render player
+        player.update(dt)
+        player.draw(screen)
+
         pygame.display.flip()
+
+        # Wait for next frame
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
