9afdd3a9215118ddc676db4dbebc01c42bd08fc7
meldridge <meldridge@users.noreply.github.com>
2024-10-03 22:43:42 +0930

    Added asteroids and asteroid field

---
+++ asteroid.py
@@ -0,0 +1,18 @@
+import pygame
+
+from circleshape import CircleShape
+from constants import *
+
+# Asteroid class
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, PLAYER_RADIUS)
+        self.velocity = 0
+
+    def update(self, dt):
+        self.position += self.velocity * dt
+    
+    def draw(self, screen):
+        pygame.draw.circle(screen, pygame.color.Color("white"), self.position, self.radius, 2)
+            
+    
--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import pygame
 # Local imports
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
@@ -21,11 +23,17 @@ def main():
     # Create groups
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
 
     # Initialise Player in the middle of the screen
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    # Initialise an Asteroid Field
+    asteroidField = AsteroidField()
 
     while(True):
         # Get events
