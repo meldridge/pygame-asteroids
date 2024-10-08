898d2373f26624ad6e072fcf74ecaca2316d1a51
Mark Eldridge <meldridge@users.noreply.github.com>
2024-10-08 20:11:16 +1000

    Fixed asteroids being created with the wrong radius

--- asteroid.py
+++ asteroid.py
@@ -6,7 +6,7 @@ from constants import *
 # Asteroid class
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
-        super().__init__(x, y, PLAYER_RADIUS)
+        super().__init__(x, y, radius)
         self.velocity = 0
 
     def update(self, dt):
